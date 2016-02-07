54d765f7312a595f21cd3e144ea7b02e4989e43a
Steve Westwood <steve.westwood@gmail.com>
2016-02-07 16:42:46 +0000

    setup

---
+++ Core/Edge.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+	public class Edge
+	{
+		public virtual int OriginID { get; set; }
+		public virtual int DestinationID { get; set; }
+	}
+}
---
+++ Core/Vertex.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+	public class Vertex
+	{
+		public virtual int ID { get; set; }
+		public virtual int Name { get; set; }
+		public virtual IEnumerable<Edge> Edges { get; set; }
+	}
+}
