4bba703ac8b119084b0ef2bbdcf7a7df98433961
Steve Westwood <steve.westwood@gmail.com>
2016-02-09 11:54:14 +0000

    consapp cont

--- Tests/ImporterTests/ImporterTests.cs
+++ Tests/ImporterTests/ImporterTests.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using Moq;
+using Newtonsoft.Json;
 using NUnit.Framework;
 
 namespace Tests.ImporterTests
@@ -8,7 +10,8 @@ namespace Tests.ImporterTests
 	[TestFixture]
 	public class EventSearchFilterServiceTests
 	{
-		private Importer.Services.ImporterService _testSubject;
+		private Importer.Services.ImporterService _importer;
+		private Importer.Services.FormatterService _formatter;
 		private Mock<Importer.Services.IPathSetter> _mockPathSetter = new Mock<Importer.Services.IPathSetter>();
 
 		[SetUp]
@@ -16,14 +19,14 @@ namespace Tests.ImporterTests
 		{
 			var correctPath = AppDomain.CurrentDomain.BaseDirectory + "ImporterTests\\XmlData\\";
 			_mockPathSetter.Setup(s => s.ReturnPath()).Returns(() => correctPath);
-			_testSubject = new Importer.Services.ImporterService(_mockPathSetter.Object);
-
+			_importer = new Importer.Services.ImporterService(_mockPathSetter.Object);
+			_formatter = new Importer.Services.FormatterService();
 		}
 
 		[Test]
 		public void Ensure_importer_returns_the_expected_amount_of_files_from_given_folder()
 		{
-			var files = _testSubject.GetFiles();
+			var files = _importer.GetFiles();
 
 			Assert.AreEqual(3, files.Count);
 		}
@@ -31,7 +34,7 @@ namespace Tests.ImporterTests
 		[Test]
 		public void Ensure_importer_returns_the_expected_files_from_given_folder()
 		{
-			var files = _testSubject.GetFiles();
+			var files = _importer.GetFiles();
 			var fileNames = new List<string>();
 			foreach (var file in files)
 			{
@@ -42,6 +45,45 @@ namespace Tests.ImporterTests
 				&& fileNames.Contains("paypal.xml");
 
 			Assert.IsTrue(allFilesFound);
+		}
+
+		[Test]
+		public void Ensure_formatter_returns_expected_objects_from_given_xml_files()
+		{
+			var expectedData = new List<Core.Vertex>();
+			// in alphabetical order
+			//amazon
+			expectedData.Add(new Core.Vertex
+			{
+				ID = 9,
+				Name = "Amazon",
+				Edges = new Core.Edge[] { new Core.Edge { OriginID = 9, DestinationID = 10 } }
+			});
+			//ibm
+			expectedData.Add(new Core.Vertex
+			{
+				ID = 4,
+				Name = "IBM",
+				Edges = new Core.Edge[] {}
+			});
+			//paypal
+			expectedData.Add(new Core.Vertex
+			{
+				ID = 5,
+				Name = "Paypal",
+				Edges = new Core.Edge[] { 
+					new Core.Edge { OriginID = 5, DestinationID = 2 },
+					new Core.Edge { OriginID = 5, DestinationID = 8 },
+					new Core.Edge { OriginID = 5, DestinationID = 5 },
+					new Core.Edge { OriginID = 5, DestinationID = 7 }
+				}
+			});
+			var files = _importer.GetFiles();
+			_formatter.InputData(files.ToArray());
+			Core.Vertex[] formattedData = _formatter.GetFormattedData();
+			var expectedJson = JsonConvert.SerializeObject((expectedData.ToArray()));
+			var actualJson = JsonConvert.SerializeObject(formattedData);
+			Assert.AreEqual(expectedJson, actualJson);
 		}
 	}
 }
