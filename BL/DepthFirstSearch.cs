93c0c8d2c6078d02e770f940c49f0a0c001dee26
Steve Westwood <steve.westwood@gmail.com>
2016-03-08 11:56:39 +0000

    depth first search implemented

---
+++ BL/DepthFirstSearch.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Core;
+
+namespace Services
+{
+    public class DepthFirstSearch
+    {
+        private readonly Vertex[] _nodes;
+        private List<int> _visitedNodes = new List<int>(); 
+        private List<List<int>> _successfulPaths = new List<List<int>>(); 
+        private List<int> _currentPath = new List<int>(); 
+        public DepthFirstSearch(Vertex[] nodes)
+        {
+            _nodes = nodes;
+        }
+
+        public int[] GetShortestPath(int sourceVertexId, int destinationVertexId)
+        {
+            var v = (from n in _nodes
+                     where n.ID == sourceVertexId
+                     select n).FirstOrDefault();
+            Search(v, destinationVertexId);
+            int length = _nodes.Length;
+            int[] path = new int[] {};
+            foreach (var p in _successfulPaths)
+            {
+                if (p.Count < length)
+                    path = p.ToArray();
+            }
+            return path;
+        }
+
+        private void Search(Vertex v, int destinationId)
+        {
+            _visitedNodes.Add(v.ID);
+            _currentPath.Add(v.ID);
+            foreach (Edge e in v.Edges)
+            {
+                if (e.DestinationID == destinationId)
+                {
+                    // do not mark destination as visited
+                    // this allows search to reach destination via different routes
+                    // add current path to successful paths (fake last step!)
+                    _currentPath.Add(e.DestinationID);
+                    _successfulPaths.Add(_currentPath.ToList());
+                    _currentPath.Remove(e.DestinationID); //reverse
+                }
+                else
+                {
+                    if (!_visitedNodes.Contains(e.DestinationID))
+                    {
+                        var w = (from n in _nodes
+                            where n.ID == e.DestinationID
+                            select n).FirstOrDefault();
+                        Search(w, destinationId);
+                    }
+                }
+            }
+            _currentPath.Remove(v.ID); //reverse
+        }
+
+    }
+}
---
+++ DepthFirstSearchConsApp/Program.cs
@@ -0,0 +1,75 @@
+﻿using Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DepthFirstSearchConsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var vertices = new List<Core.Vertex>
+            {
+                new Core.Vertex
+                {
+                    ID = 1,
+                    Name = "1",
+                    Edges = new[]
+                    {
+                        new Core.Edge {OriginID = 1, DestinationID = 2},
+                        new Core.Edge {OriginID = 1, DestinationID = 3}
+                    }
+                },
+                new Core.Vertex
+                {
+                    ID = 2,
+                    Name = "2",
+                    Edges = new[]
+                    {
+                        new Core.Edge {OriginID = 2, DestinationID = 4},
+                        new Core.Edge {OriginID = 2, DestinationID = 5}
+                    }
+                },
+                new Core.Vertex
+                {
+                    ID = 3,
+                    Name = "3",
+                    Edges = new[]
+                    {
+                        new Core.Edge {OriginID = 3, DestinationID = 6}
+                    }
+                },
+                new Core.Vertex
+                {
+                    ID = 4,
+                    Name = "4",
+                    Edges = new[]
+                    {
+                        new Core.Edge {OriginID = 4, DestinationID = 1},
+                        new Core.Edge {OriginID = 4, DestinationID = 6}
+                    }
+                },
+                new Core.Vertex
+                {
+                    ID = 5,
+                    Name = "5",
+                    Edges = new Core.Edge[] {}
+                },
+                new Core.Vertex
+                {
+                    ID = 6,
+                    Name = "6",
+                    Edges = new Core.Edge[] {}
+                }
+            };
+
+            DepthFirstSearch dfs = new DepthFirstSearch(vertices.ToArray());
+            var path = dfs.GetShortestPath(1, 6);
+
+
+        }
+    }
+}
